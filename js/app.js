aec16b2298a26c7a2197bd73bfcb4686922cbe29
Your Name <you@example.com>
2024-09-03 08:21:17 -0500

    drawer

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 const hamburger = document.querySelector('.header__hamburger')
+const logo = document.querySelector('.nav__logo')
 const drawerMenu = document.querySelector('.nav__list:nth-child(2)')
 
 hamburger.addEventListener('click', () => {
     drawerMenu.classList.toggle('nav__list--show')
+    logo.classList.toggle('nav__logo--active')
 });
 
 
 document.addEventListener('click', (e) => {
     if (!drawerMenu.contains(e.target) && !hamburger.contains(e.target)) {
         drawerMenu.classList.remove('nav__list--show');
-        console.log(e.target);
+        logo.classList.remove('nav__logo--active')
         
     }
 });
